3baf741bdc624fd21d43060240632d69c498165a
Dilmith-Ranasinghe518 <it23249998@my.sliit.lk>
2026-08-14 16:07:57 +0530

    feat: update sort_order to TextInputColumn for inline editing in PortfolioItemResource

--- app/Filament/Resources/PortfolioItemResource.php
+++ app/Filament/Resources/PortfolioItemResource.php
@@ -387,8 +387,10 @@ class PortfolioItemResource extends Resource
                 Tables\Columns\ToggleColumn::make('show_gallery')
                     ->label('Gallery')
                     ->sortable(),
-                Tables\Columns\TextColumn::make('sort_order')
-                    ->sortable()->label('Order'),
+                Tables\Columns\TextInputColumn::make('sort_order')
+                    ->numeric()
+                    ->sortable()
+                    ->label('Order'),
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('categories')
